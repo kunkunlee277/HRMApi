43068f266e65b95be3bc5bd2bca99d56a64eca71
AnhVN <anhvn@histaff.vn>
2024-11-29 10:37:33 +0700

    test connect

---
+++ HRMApi/HRMApi/Controllers/TestConnectionController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace HRMApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestConnectionController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TestConnectionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnecttion");
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (var command = new SqlCommand("SELECT 1", connection))
+                    {
+                        var result = command.ExecuteScalar();
+                        return Ok("Connection successful. Test query result: " + result);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Connection failed: " + ex.Message);
+            }
+        }
+    }
+}
